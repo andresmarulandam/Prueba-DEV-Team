ef2e5f788d746dbf6d8cd4d70d89543248c41de9
Andres M <marulandaandres19@gmail.com>
2024-03-20 12:57:21 -0500

    setup api, axios, .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { fetchData } from './services/api';
 
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const test = async () => {
+      try {
+        const response = await fetchData();
+        console.log('Datos recibidos del Api:', response.data);
+      } catch (error) {
+        console.error('Error', error);
+      }
+    };
+    test();
+  }, []);
+
   return (
     <>
       <h1>Hola</h1>
---
+++ src/services/api.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const api = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    'app-id': apiKey,
+  },
+});
+
+export const fetchData = async () => {
+  try {
+    const response = await api.get('user', {
+      params: {
+        page: 1,
+        limit: 10,
+      },
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+export const fetchPostById = (postId) => {
+  return axios.get(`${apiUrl}/post/${postId}`, {
+    headers: {
+      'app-id': apiKey,
+    },
+  });
+};
+
+export const fetchCommentsByPostId = (postId) => {
+  return axios.get(`${apiUrl}/post/${postId}/comment`, {
+    headers: {
+      'app-id': apiKey,
+    },
+  });
+};
